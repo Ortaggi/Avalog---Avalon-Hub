78f7f3cb3b11c6dd544f22a53a4e1e883950b02c
Diego Gettatelli <dgettatelli@gmail.com>
2025-12-04 22:10:45 +0100

    feat: add navbar and styles.scss

--- avalog-fe/src/app/app.component.ts
+++ avalog-fe/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+import {SharedModule} from "./shared/shared.module";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
