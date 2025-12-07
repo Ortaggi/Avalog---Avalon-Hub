292edcee52617cd420e61eeae601dc26874b61d6
Diego Gettatelli <dgettatelli@gmail.com>
2025-12-07 17:23:10 +0100

    feat(auth-service): add auth service method

--- avalog-fe/src/app/core/services/auth.service.ts
+++ avalog-fe/src/app/core/services/auth.service.ts
@@ -18,7 +18,21 @@ export class AuthService {
     createdAt: new Date('2025-12-06')
   };
 
+  // TODO: Capire se con i signal e` giusto o meno
+  getCurrentUser(): User | null {
+    return this.currentUser();
+  }
+
   login(): boolean {
+    return false;
+  }
+
+  logout(): void {
+    this.currentUser.set(null);
+    this.isAuthenticated.set(false);
+  }
+
+  register(): boolean {
     return false;
   }
 }
