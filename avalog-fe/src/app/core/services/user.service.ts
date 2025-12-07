14c01708955183b7b4b31b5f4305da5f641e6db9
Diego Gettatelli <dgettatelli@gmail.com>
2025-12-07 17:31:34 +0100

    feat(user-service): add user service methods

--- avalog-fe/src/app/core/services/user.service.ts
+++ avalog-fe/src/app/core/services/user.service.ts
@@ -1,6 +1,44 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {}
+export class UserService {
+  // Aggiungo alcuni utenti MOCK
+  private users: User[] = [
+    {
+      id: '1',
+      email: 'mariorossi@email.it',
+      username: 'mrossi66',
+      displayName: 'Mario Rossi',
+      createdAt: new Date('2024-12-01')
+    },
+    {
+      id: '2',
+      email: 'toniocartonio@email.it',
+      username: 'toniocartonio10',
+      displayName: 'Tonio Cartonio',
+      createdAt: new Date('2024-12-02')
+    },
+    {
+      id: '3',
+      email: 'ninofrassica@email.it',
+      username: 'ninofrassica1',
+      displayName: 'Tony New Star Dance 2000',
+      createdAt: new Date('2024-12-03')
+    }
+  ];
+
+  getAll(): User[] {
+    return this.users;
+  }
+
+  getById(id: string): User | undefined {
+    return this.users.find((u) => u.id === id);
+  }
+
+  getByUsername(username: string): User | undefined {
+    return this.users.find((u) => u.username === username);
+  }
+}
