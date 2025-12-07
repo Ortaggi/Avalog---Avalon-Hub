e069877ce0877b68335b955c9822424d9b06cd8c
Diego Gettatelli <dgettatelli@gmail.com>
2025-12-07 22:26:25 +0100

    fix(navbar): edit navabar show
    now navbar is showed only if user is authenticated

--- avalog-fe/src/app/app.component.ts
+++ avalog-fe/src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
-import {SharedModule} from "./shared/shared.module";
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { AuthService } from './core/services';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, SharedModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
 export class AppComponent {
+  authService: AuthService = inject(AuthService);
   title = 'avalog-fe';
 }
