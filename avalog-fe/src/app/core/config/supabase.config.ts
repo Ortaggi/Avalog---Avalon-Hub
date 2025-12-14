7c061f2df1d9f47e87d19803e11ef8b2f6aec474
Diego Gettatelli <dgettatelli@gmail.com>
2025-12-14 22:39:28 +0100

    feat: add file to gitignore

--- avalog-fe/src/app/core/config/supabase.config.ts
+++ avalog-fe/src/app/core/config/supabase.config.ts
@@ -1,5 +1,6 @@
+import { appConfig } from './index';
+
 export const SUPABASE_CONFIG = {
-  url: 'https://flfoyhmtxcputsxohsvf.supabase.co',
-  anonKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsZm95aG10eGNwdXRzeG9oc3ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2MjczMzgsImV4cCI6MjA4MTIwMzMzOH0.b1Dud2hWbP0gkWLovKM1WLcPbkDDOLrvk6gKIiX2aIw'
+  url: appConfig.SUPABASE_URL,
+  anonKey: appConfig.SUPABASE_ANON_KEY
 };
